11051a3a3292efc6cc8af032a0f168228c0591a1
ar612252 <anthony.rosas.995@my.csun.edu>
2024-03-15 21:25:46 -0700

    Firstcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloComponent } from "./hello/hello.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HelloComponent, HttpClientModule]
 })
 export class AppComponent {
+  
+
+
   title = 'weatherClient';
 }
